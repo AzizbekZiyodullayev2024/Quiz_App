84544eec4d0c618f7a4c0d1cbde05e391614b5e6
AzizbekZiyodullayev2024 <ziyodulayeva@gmail.com>
2025-01-10 20:40:38 +0500

    HW

--- resources/views/auth/register.php
+++ resources/views/auth/register.php
@@ -65,5 +65,25 @@
         </form>
     </div>
 </div>
+
+<script>
+
+    async function register() {
+        let form = document.getElementById("register"),
+            formData = new FormData(form);
+        const { default: apiFetch } = await import('./js/utils/apiFetch.js');
+
+        await apiFetch('./register', { method: 'POST', body: formData })
+            .then(data => console.log(data))
+            .catch((error) => {
+                console.error(error.data.errors);
+                Object.keys(error.data.errors).forEach(err => {
+                    document.getElementById('error').innerHTML += `<p class="text-red-500 mt-1">${error.data.errors[err]}</p>`;
+                });
+            });
+    }
+</script>
+
+
 </body>
 </html>
