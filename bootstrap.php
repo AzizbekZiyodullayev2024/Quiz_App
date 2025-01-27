1abdf63c66bb2aebffdfae3c6965569e4df7b25b
AzizbekZiyodullayev2024 <ziyodulayeva9@gmail.com>
2025-01-28 00:43:40 +0500

    HW

--- bootstrap.php
+++ bootstrap.php
@@ -4,5 +4,5 @@ require 'vendor/autoload.php';
 date_default_timezone_set('Asia/Tashkent');
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
-
+$php = 45;
 require 'helpers.php';
