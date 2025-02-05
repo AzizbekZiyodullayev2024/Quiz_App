7e2f3dfd7ec73731d318f2ced183802094febe92
AzizbekZiyodullayev2024 <ziyodulayeva9@gmail.com>
2025-02-05 20:37:35 +0500

    HW

--- resources/views/home.php
+++ resources/views/home.php
@@ -1,4 +1,5 @@
 <?php require '../resources/views/components/dashboard/header.php'?>
+
 <body class="bg-gradient-to-b from-gray-50 to-gray-100">
 <nav class="bg-white shadow-lg">
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
